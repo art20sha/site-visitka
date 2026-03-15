f9ae279b3f0cad1aae991653393ec78c2d199036
art20sha <shatinartem2011@gmail.com>
2026-03-15 14:03:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
   const text = `Новое сообщение с сайта:%0AИмя: ${name}%0AEmail: ${email}%0AСообщение: ${message}`;
 
   const token = "ВАШ_ТОКЕН_БОТА";
-  const chat_id = "ВАШ_CHAT_ID";
+  const chat_id = "5925170010";
+
+  const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}`;
+
+  fetch(url)
+  .then(response => {
+    document.getElementById("status").innerText = "Сообщение отправлено!";
+  })
+  .catch(error => {
+    document.getElementById("status").innerText = "Ошибка отправки";
+  });
+});
 
   const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}`;
 
